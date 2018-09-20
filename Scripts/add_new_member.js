4d7632bd730b8afaed7541ef4645bbddab698e50
=minh656644 <=minh656644@gmail.com>
2018-09-20 19:59:40 +0700

    update

--- Scripts/add_new_member.js
+++ Scripts/add_new_member.js
@@ -56,9 +56,32 @@
         }
     });
 
+    // hiện madal 
     $('.button-country').click(function () {
+        $(this).addClass('processing');
+        $("#table").load("Views/table-country.html", function () {
+
+            $(".btn-close").click(function () {
+                $('.processing').removeClass('processing');
+                $('#table').empty();
+            });
+            $(".button-close-madal").click(function () {
+                $('.processing').removeClass('processing');
+                $('#table').empty();
+            });
+        });
+    });
+
+    // hiện madal 
+    $('.button-relationship').click(function () {
+        $(this).addClass('processing');
         $("#table").load("Views/table-relationship.html", function () {
             $(".btn-close").click(function () {
+                $('.processing').removeClass('processing');
+                $('#table').empty();
+            });
+            $(".button-close-madal").click(function () {
+                $('.processing').removeClass('processing');
                 $('#table').empty();
             });
         });
--- Scripts/table-country.js
+++ Scripts/table-country.js
@@ -1,47 +1,54 @@
-﻿$.ajax({
+﻿// tải dữ liệu country vào bảng
+$.ajax({
     type: 'GET',
-    url: 'Scripts/data-address/relationship.json',
+    url: 'Scripts/data-address/country.json',
     success: function (data) {
         $.each(data, function (index, value) {
             // APPEND OR INSERT DATA TO SELECT ELEMENT. Set the country code in the id section rather than in the value.
-            $("#table-list-relationship").append(
-                '<tr> <td>' + value.relationship_name + '</td> <td>' + value.relationshipg_group + '</td> <td>' + value.note + '</td></tr>'
+            $("#table-list-country").append(
+                '<tr> <td>' + value.code.toUpperCase() +'</td> <td>' + value.name + '</td> <td><span class="box-check unchecked"></span></td></tr>'
             );
         });
 
-        $('.table-select-relationship').find("input").first().on("keyup", function () {
+        // lọc code-country theo giá trị nhập vào từ bán phím
+        $('.table-select-country').find("input").first().on("keyup", function () {
             var value = $(this).val().toLowerCase();
-            $('#table-list-relationship').find("tr").filter(function () {
+            $('#table-list-country').find("tr").filter(function () {
                 value = ascii_slug(value);
                 $(this).toggle(ascii_slug($(this).children().first().text().toLowerCase()).indexOf(value) > -1);
             });
         });
 
-        $('.table-select-relationship').find("input:odd").on("keyup", function () {
+        // lọc name-country theo giá trị nhập vào từ bàn phím
+        $('.table-select-country').find("input").last().on("keyup", function () {
             var value = $(this).val().toLowerCase();
-            debugger;
-            $('#table-list-relationship').find("tr").filter(function () {
+            $('#table-list-country').find("tr").filter(function () {
                 value = ascii_slug(value);
                 $(this).toggle(ascii_slug($(this).children().first().next().text().toLowerCase()).indexOf(value) > -1);
             });
         });
 
-        $('.table-select-relationship').find("input").last().on("keyup", function () {
-            var value = $(this).val().toLowerCase();
-            $('#table-list-relationship').find("tr").filter(function () {
-                value = ascii_slug(value);
-                $(this).toggle(ascii_slug($(this).children().last().text().toLowerCase()).indexOf(value) > -1);
-            });
-        });
-
-        $('#table-list-relationship').find('tr').click(function () {
-            $('#table-list-relationship').find('tr').removeClass('select-tr');
+        // thêm select vào hàng country khi ấn 
+        $('#table-list-country').find('tr').click(function () {
+            $('#table-list-country').find('tr').removeClass('select-tr');
             $(this).addClass('select-tr');
+            if ($(this).find('.box-check').hasClass('checked')) {
+                $(this).find('.box-check').toggleClass('checked');
+                $(this).find('.box-check').toggleClass('unchecked');
+            }
+            else {
+                $('tbody').find('.box-check').removeClass('checked');
+                $('tbody').find('.box-check').addClass('unchecked');
+                $(this).find('.box-check').toggleClass('checked');
+                $(this).find('.box-check').toggleClass('unchecked');
+            }
         });
 
+        // Án nút chọn thì thoát madel
         $('.button-madal-select-country').click(function () {
-            var tr_select = $('#table-list-relationship').find('.select-tr');
-            $('.processing').siblings('input').val(tr_select.children().first().text());
+            var tr_select = $('#table-list-country').find('.select-tr');
+            $('.processing').siblings('input').val(tr_select.children().first().next().text());
+            $('.processing').siblings('input').attr('code-country', tr_select.children().first().text().toLowerCase());
             $('.processing').siblings('input').focus();
             $('.processing').removeClass('processing');
             $('#table').empty();
@@ -49,6 +56,7 @@
     },
 });
 
+// phóng to madal
 $('#btn-maxsize-restore').click(function () {
     if ($(this).hasClass('btn-maxsize')) {
         $(this).parents('.modal-contents').removeClass('modal-size');
@@ -61,6 +69,7 @@ $('#btn-maxsize-restore').click(function () {
     }
 });
 
+// phóng to madal
 $('.modal-header').dblclick(function () {
     if ($(this).parent().hasClass('modal-size')) {
         $(this).parent().removeClass('modal-size');
@@ -73,4 +82,4 @@ $('.modal-header').dblclick(function () {
     }
 });
 
-
+    
